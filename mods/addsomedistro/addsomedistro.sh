a4ede063e1720d4b4996ab97d7f02529c8e491dd
I'm Complete The Opposite <113910432+ImCompleteOpposite@users.noreply.github.com>
2025-06-12 14:11:01 +0800

    Update addsomedistro.sh

--- mods/addsomedistro/addsomedistro.sh
+++ mods/addsomedistro/addsomedistro.sh
@@ -4,4 +4,10 @@ wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs
 wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/ubujammy.sh
 wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/ubufocal.sh
 wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/ubulunar.sh 
+wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/fedora41.sh 
+wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/fedora40.sh
+clear
+echo "Installation Finished."
+sleep 2
+clear
 proot-distro list
