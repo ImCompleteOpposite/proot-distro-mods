37d377f6bbadf7db8d917aa6c3f83718615b290e
I'm Complete The Opposite <113910432+ImCompleteOpposite@users.noreply.github.com>
2025-06-12 13:16:05 +0800

    Update addsomedistro.sh

--- mods/addsomedistro/addsomedistro.sh
+++ mods/addsomedistro/addsomedistro.sh
@@ -1,3 +1,3 @@
 cd $PREFIX/etc/proot-distro/
-wget https://github.com/ImCompleteOpposite/proot-distro-mods/mods/addsomedistro/debullseye.sh
+wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/debullseye.sh
 proot-distro list
