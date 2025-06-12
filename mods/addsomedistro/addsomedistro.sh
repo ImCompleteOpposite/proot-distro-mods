381089dd8e86bb0e03c3874bf5ce955755158383
I'm Complete The Opposite <113910432+ImCompleteOpposite@users.noreply.github.com>
2025-06-12 14:01:14 +0800

    Update addsomedistro.sh

--- mods/addsomedistro/addsomedistro.sh
+++ mods/addsomedistro/addsomedistro.sh
@@ -3,4 +3,5 @@ wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs
 wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/ubumantis.sh
 wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/ubujammy.sh
 wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/ubufocal.sh
+wget https://raw.githubusercontent.com/ImCompleteOpposite/proot-distro-mods/refs/heads/main/mods/addsomedistro/ubulunar.sh 
 proot-distro list
